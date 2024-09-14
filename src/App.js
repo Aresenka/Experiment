3c4181d171b942790ed9b669aeb1cf546eaeade9
Kirill Chernenko <kirill.chernenko@paxful.com>
2024-09-14 19:53:49 +0400

    Init React app commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+      <div style={{ textAlign: 'center', padding: '50px' }}>
+        <h1>Welcome to Scilef Experiments Bot!</h1>
+        <p>This is a prototype of a game-building experiment where players decide the future of the game.</p>
+        <a href="https://t.me/scilef_games">Subscribe our channel</a>
+      </div>
+  );
+}
+
+export default App;
