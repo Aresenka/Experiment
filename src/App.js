076a3016f70904caa1412e8099f53f7b32b418c3
Kirill Chernenko <kirill.chernenko@paxful.com>
2024-09-14 19:59:10 +0400

    Typo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
       <div style={{ textAlign: 'center', padding: '50px' }}>
         <h1>Welcome to Scilef Experiments Bot!</h1>
         <p>This is a prototype of a game-building experiment where players decide the future of the game.</p>
-        <a href="https://t.me/scilef_games">Subscribe our channel</a>
+        <a href="https://t.me/scilef_games">Subscribe to our channel</a>
       </div>
   );
 }
